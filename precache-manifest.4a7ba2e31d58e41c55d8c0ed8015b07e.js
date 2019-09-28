e507f4daf3aa9c9d4c4a07ef39452578d2d7c222
Anna Kholod <uashop89@gmail.com>
2019-09-28 11:06:15 +0300

    Updates

---
+++ precache-manifest.4a7ba2e31d58e41c55d8c0ed8015b07e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203b27e9e817f1639b4d8c913ec45d82",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "e87a87960bd675748126",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "89eb379c6e770c53a87c",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.072b134e.chunk.css"
+  },
+  {
+    "revision": "e87a87960bd675748126",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.14594142.chunk.js"
+  },
+  {
+    "revision": "89eb379c6e770c53a87c",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.2033281d.chunk.js"
+  },
+  {
+    "revision": "93e0768f7654193c6b2a",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.0a866071.js"
+  }
+]);
